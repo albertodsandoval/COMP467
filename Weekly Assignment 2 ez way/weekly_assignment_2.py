121d3692071fd50932e0c38275415c2c7e3df4a7
Alberto Sandoval <141599770+albertodsandoval@users.noreply.github.com>
2025-02-18 09:37:22 -0800

    Update weekly_assignment_2.py

--- Weekly Assignment 2 ez way/weekly_assignment_2.py
+++ Weekly Assignment 2 ez way/weekly_assignment_2.py
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
 import re
 with open("ingest_this.txt","r") as file:
 	for line in file:
-=======
 import re
 with open("ingest_this.txt","r") as file:
 	for line in file:
->>>>>>> master
 		print(re.findall(r'\d+',line))	
