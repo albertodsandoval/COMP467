23c0652129a08bb56599c1a69c58b092d2ca040d
AlbertoSandovalCSUN <141599770+AlbertoSandovalCSUN@users.noreply.github.com>
2025-03-14 13:58:36 -0700

    rebase

---
+++ Weekly Assignment 4/Weekly_Assignment_4.py
@@ -0,0 +1,16 @@
+import pymongo
+
+MONGO_URI = "mongodb+srv://albertosandoval950:y6fmhzmwJMY0kZV9@users.a7kxh.mongodb.net/?retryWrites=true&w=majority&appName=Users"
+
+myclient = pymongo.MongoClient(MONGO_URI)
+
+mydb = myclient["LevelUp"]
+
+
+collection = mydb["users"]
+
+mydict = {"username":"John","password":"password"}
+
+x = collection.insert_one(mydict)
+
+print(collection.find_one({"username":"John"}))
